c2b7f9277cfa1fa021a5f8e8f1e1ffcb3bb61173
Andrés Felipe Mesa David <andres.david@coinsquare.com>
2021-07-15 21:48:56 -0500

    Challenge of chapters 4,6,8 and ADD a .gitignore

---
+++ ch8/factors.py
@@ -0,0 +1,16 @@
+# 8.4 Find the factors of a number
+
+def factors(num):
+    for x in range(1, num + 1):
+        if num % x:
+            continue
+        else:
+            print(f"{x} is a factor of {num}")
+
+def run():
+    num = int(input("Enter a positive integer: "))
+    factors(num)
+
+
+if __name__ == '__main__':
+    run()
