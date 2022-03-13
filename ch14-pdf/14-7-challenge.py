2f708b3f3b54f862addaa8f8ce313fc41383640c
Andres <andresf.mesad@gmail.com>
2022-03-13 00:41:38 -0500

    Add ch15 exercises

---
+++ ch14-pdf/14-7-challenge.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
+
+def get_page_text(page):
+    return page.extractText()
+
+def main():
+    pdf_path = Path().cwd() / "ch14-pdf" / "practice_files" / "scrambled.pdf"
+    output_pdf = Path().cwd() / "ch14-pdf" / "unscrambled.pdf"
+    pdf_reader = PdfFileReader(str(pdf_path))
+    pdf_writer = PdfFileWriter()
+
+
+    pdf_reader = PdfFileReader(str(pdf_path))
+    pdf_writer = PdfFileWriter()
+
+    pages = list(pdf_reader.pages)
+    pages.sort(key=get_page_text)
+
+    for page in pages:
+        rotation_degrees = page["/Rotate"]
+        if rotation_degrees != 0:
+            page.rotateCounterClockwise(rotation_degrees)
+        pdf_writer.addPage(page)
+
+    with output_pdf.open(mode="wb") as output_file:
+        pdf_writer.write(output_file)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ ch15-databases/15-1-intro_sql.py
@@ -0,0 +1,41 @@
+import sqlite3
+from pathlib import Path
+
+
+def main():
+    
+    values = (
+        ("Benjamin Sisko", "Human", 40),
+        ("Jadzia Dax", "Trill", 300),
+        ("Kira Nerys", "Bajoran", 29),
+    )
+
+    create_table = """
+    CREATE TABLE IF NOT EXISTS Roster (
+        Name TEXT,
+        Species TEXT,
+        Age INT
+    );
+    """
+
+    insert_values = "INSERT INTO Roster VALUES(?,?,?);"
+
+    db_path = Path().cwd() / "ch15-databases" / "Roster.db"
+
+    with sqlite3.connect(db_path) as connection:
+        cursor = connection.cursor()
+        # Create table
+        cursor.execute(create_table)
+        # Insert values in the table
+        cursor.executemany(insert_values, values)
+        # Update name in table
+        cursor.execute("UPDATE Roster SET Name=? WHERE Name=?", ("Ezri Dax", "Jadzia Dax"))
+        # Display the name and age of everyone in the table classified as Bajoran
+        cursor.execute("SELECT Name, Age FROM Roster")
+        for row in cursor.fetchall():
+            print(row)
+
+
+
+if __name__ == '__main__':
+    main()
